12a9849ff07b7b29ccc8e4f826af56395d762ad5
moona8 <73126379+moona8@users.noreply.github.com>
2021-11-20 17:27:45 +0500

    deploy

--- src/redux/todo.js
+++ src/redux/todo.js
@@ -6,6 +6,7 @@ export const todoSlice = createSlice({
   initialState: {
     todos: [],
   },
+  
   reducers: {
     addTodo: (state, action) => {
       state.todos.push(action.payload)
@@ -20,6 +21,7 @@ export const todoSlice = createSlice({
       }
   },
 });
+
 
 export const { addTodo,deleteTodo ,deleteAll} = todoSlice.actions;
 export default todoSlice.reducer;
